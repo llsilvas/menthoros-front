e0aa461885d8185bfd96502234a0d9641314c234
Leandro Silva <lsilva.info@gmail.com>
2025-10-21 19:54:38 -0300

    feat(dashboard): cabeçalho de seção na sidebar

---
+++ src/components/dashboard/DashboardSidebarHeaderItem.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import ListSubheader from '@mui/material/ListSubheader';
+import type {} from '@mui/material/themeCssVarsAugmentation';
+import DashboardSidebarContext from '../../context/DashboardSidebarContext';
+import { DRAWER_WIDTH } from '../../constants/constants';
+import { getDrawerSxTransitionMixin } from '../../utils/mixins';
+
+export interface DashboardSidebarHeaderItemProps {
+  children?: React.ReactNode;
+}
+
+export default function DashboardSidebarHeaderItem({
+  children,
+}: DashboardSidebarHeaderItemProps) {
+  const sidebarContext = React.useContext(DashboardSidebarContext);
+  if (!sidebarContext) {
+    throw new Error('Sidebar context was used without a provider.');
+  }
+  const {
+    mini = false,
+    fullyExpanded = true,
+    hasDrawerTransitions,
+  } = sidebarContext;
+
+  return (
+    <ListSubheader
+      sx={{
+        fontSize: 11,
+        fontWeight: '700',
+        textTransform: 'uppercase',
+        letterSpacing: '0.5px',
+        height: mini ? 0 : 36,
+        ...(hasDrawerTransitions
+          ? getDrawerSxTransitionMixin(fullyExpanded, 'height')
+          : {}),
+        px: 1.5,
+        py: 0,
+        minWidth: DRAWER_WIDTH,
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        zIndex: 2,
+        backgroundColor: 'transparent',
+        color: '#b1e92d', // Verde secundário do Menthoros
+        lineHeight: '36px',
+      }}
+    >
+      {children}
+    </ListSubheader>
+  );
+}
